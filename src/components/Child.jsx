bee49b2ec14a83cd68816666be977962080974e0
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-10-22 12:20:57 +0530

    final

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -13,10 +13,10 @@ function Child() {
   };
 
   return (
-    <div>
-      <div className="row">
+    <div className="container-fluid px-0" id='boxs'>
+      <div className="row justify-content-center" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
         {context.products.map(product => (
-          <div key={product.id} >
+          <div key={product.id} className="col-12 mx-auto px-0">
             <div id="box" className="card-group" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               <h1 className="card-title text-center mt-3"> {product.brand}</h1><br />
               <img className="card-img d-block mt-3 mx-3 " style={{ maxWidth: '200px', maxHeight: '200px' }} src={product.thumbnail} alt={product.title} />
@@ -28,9 +28,9 @@ function Child() {
                   {`Discount: ${product.discountPercentage}%`}<br />
                   {`Category: ${product.category}`}<br />
                   {`Stock: ${product.stock}`}
-                            <h2>Price : ${ product.price}</h2>
-                        </p>
-                        <label><h4 className='mx-5'>Quantity : </h4></label>
+                  <h2>Price : ${ product.price}</h2>
+                </p>
+                <label><h4 className='mx-5'>Quantity : </h4></label>
                 <select className='card mb-3 d-inline-block' onChange={(event) => handleChange(product.id, product.price, event)}>
                   <option value='1'>1</option>
                   <option value='2'>2</option>
